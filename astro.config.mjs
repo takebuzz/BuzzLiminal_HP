e488748207a7be20d20c7d896d027905eebd2a78
小菅秀二 <kosugeshuji@kosugahidejinoMacBook-Air.local>
2025-09-08 16:51:04 +0900

    セキュリティ関連、モバイル版でのレイアウト修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.buzzliminal.co.jp',
   integrations: [sitemap()],
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     css: {
       postcss: './postcss.config.mjs'
